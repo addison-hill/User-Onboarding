38e7282cee418fe624338365fb3c53de97df5ead
Addison Hill <adddison.hill@gmail.com>
2019-10-17 16:45:46 -0500

    created form, working on validation

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import UserForm from './UserForm';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Addison Hill</h1>
+      <UserForm />
     </div>
   );
 }
---
+++ user-onboarding/src/UserForm.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+
+const UserForm = ({ errors, touched, values, status }) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        status && setUsers(users => [...users, status]);
+    }, [status]);
+
+    return (
+        <div className="user-form">
+            <h1>Sign In</h1>
+            <Form>
+                <Field type="text" name="name" placeholder="Name" />
+                {touched.name && errors.name && (
+                    <p className="error">{errors.name}</p>
+                )}
+
+                <Field type="text" name="email" placeholder="Email" />
+                {touched.email && errors.email && (
+                    <p className="error">{errors.email}</p>
+                )}
+
+                <Field type="text" name="password" placeholder="Password" />
+                {touched.password && errors.password && (
+                    <p className="error">{errors.password}</p>
+                )}
+
+                <label className="check-box-container">
+                    Terms of Service
+                    <Field 
+                        type="checkbox"
+                        name="terms"
+                        checked={values.terms}
+                    />
+                    <span className="checkmark" />
+                </label>
+
+                <button type="submit">Submit!</button>
+            </Form>
+
+            {users.map(user => (
+                <ul key={user.id}>
+                    <li>Name: {user.name}</li>
+                    <li>Email: {user.email}</li>
+                    <li>Password: {user.password}</li>
+                </ul>
+            ))}
+        </div>
+    );
+};
+
+const FormikUserForm = withFormik({
+    mapPropsToValues({ name, email, password, terms }) {
+        return {
+            name: name || "",
+            email: email || "",
+            password: password || "",
+            terms: terms || false
+        };
+    },
+
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required("Name required"),
+        email: 
+    })
+})
+    
+export default UserForm
